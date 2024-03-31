e16d88dadc45865d89a4ee16d173a7c444cc0f9e
Roman Gontar <ashenoooone@gmail.com>
2024-03-31 16:48:54 +0400

    fix фикс сборки

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import './styles/index.css';
 import { Header } from '~/widgets/Header';
 import { RouterProvider } from '~/app/providers/RouterProvider';
 import { Footer } from '~/widgets/Footer/Footer.tsx';
+import React from 'react';
 
 function App() {
   return (
